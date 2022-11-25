2b0a6ad6ba1daf0e15151531129e44e4f0f1b2a7
mangesh <mangeshdb108@gmail.com>
2022-11-25 13:35:58 +0530

    path change

--- pages/about.js
+++ pages/about.js
@@ -17,7 +17,7 @@ export default function about() {
   return (
     <>
         
-<section className="page-title parallaxie" data-bg-img="/biogx/images/bg/biogx_about.jpg" style={{  backgroundImage: `url("/images/bg/biogx_about.jpg")`  }}>
+<section className="page-title parallaxie" data-bg-img="/biogx/images/bg/biogx_about.jpg" style={{  backgroundImage: `url("/biogx/images/bg/biogx_about.jpg")`  }}>
   <div className="container">
     <div className="row">
       <div className="col-lg-6">
